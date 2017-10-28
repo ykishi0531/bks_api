18d5078629f2ec1469044ef108d2ec08b8d243c7
ykishi0531 <ykishi0531@gmail.com>
2017-10-28 09:11:57 +0000

    xml totyuu

--- national_lib.py
+++ national_lib.py
@@ -1,4 +1,6 @@
 import urllib.request
+import re
+from xml.etree import ElementTree
 
 
 api_url = 'http://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=isbn='
@@ -6,8 +8,11 @@ api_url = 'http://iss.ndl.go.jp/api/sru?operation=searchRetrieve&query=isbn='
 
 def nl_isbn(isbn):
 	global api_url
-	print(api_url)
-	res = urllib.request.urlopen(api_url + isbn)
 	
-	return res.read()
+	res = urllib.request.urlopen(api_url + isbn)
+	xml_data = res.read()
+	xml_data = re.sub(r'\d', 'l', xml_data)
+	
+	root = ElementTree.fromstring(xml_data)
+	return xml_data
 
